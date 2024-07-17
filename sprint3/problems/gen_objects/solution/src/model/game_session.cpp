efc0c6879d98622031cff3e0f3567982cd7601b6
zonner27 <zonner2785@gmail.com>
2024-07-17 14:56:18 +0300

    gen object refactor code

--- sprint3/problems/gen_objects/solution/src/model/game_session.cpp
+++ sprint3/problems/gen_objects/solution/src/model/game_session.cpp
@@ -36,179 +36,124 @@ std::unordered_set<std::shared_ptr<LostObject> > &GameSession::GetLostObject() n
 }
 
 void GameSession::UpdateSessionByTime(const std::chrono::milliseconds& time_delta) {
-     try {
-        UpdateDogsCoordinatsByTime(time_delta);
-        UpdateLootGenerationByTime(time_delta);
-    } catch (const std::exception& e) {
-            // Ловим исключения std::exception и добавляем информацию о функции
-            throw std::runtime_error(std::string("Exception in UpdateSessionByTime: ") + e.what());
-    } catch (...) {
-        // Ловим все остальные исключения и добавляем информацию о функции
-        throw std::runtime_error("Unknown exception in UpdateSessionByTime");
-    }
+
+    UpdateDogsCoordinatsByTime(time_delta);
+    UpdateLootGenerationByTime(time_delta);
+
 }
 
 void GameSession::UpdateDogsCoordinatsByTime(const std::chrono::milliseconds& time_delta_ms){
 
-    try {
+    int time_delta = static_cast<int>(time_delta_ms.count());
 
-        int time_delta = static_cast<int>(time_delta_ms.count());
+    for (auto& dog : dogs_) {
+        Coordinates start = dog->GetCoordinate();
+        Dimension start_x = static_cast<Dimension>(std::round(start.x));
+        Dimension start_y = static_cast<Dimension>(std::round(start.y));
 
-        //net::dispatch(*game_session_strand_, [self = shared_from_this(), &time_delta]() {
-
-        for (auto& dog : dogs_) {
-            Coordinates start = dog->GetCoordinate();
-            Dimension start_x = static_cast<Dimension>(std::round(start.x));
-            Dimension start_y = static_cast<Dimension>(std::round(start.y));
-
-            Coordinates calc_finish = dog->GetCoordinateByTime(time_delta);
-            Coordinates finish;
-            constants::Direction direction = dog->GetDirection();
-            if (direction == constants::Direction::EAST || direction == constants::Direction::WEST) {
-                finish.y = start.y;
-                if (const Road* road_cur = map_->GetHorRoad(start_x, start_y)) {
-                    if (direction == constants::Direction::EAST) {
-                        double road_x_end = static_cast<double>(road_cur->GetEnd().x);
-                        if (calc_finish.x - road_x_end <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.x = calc_finish.x;
-                        } else {
-                            finish.x = road_x_end + constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
-                    }
-                    if (direction == constants::Direction::WEST) {
-                        double road_x_begin = static_cast<double>(road_cur->GetStart().x);
-                        if (road_x_begin - calc_finish.x <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.x = calc_finish.x;
-                        } else {
-                            finish.x = road_x_begin - constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
-                    }
-                } else {
-                    if (direction == constants::Direction::EAST) {
-                        if (calc_finish.x - static_cast<double>(start_x) <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.x = calc_finish.x;
-                        } else {
-                            finish.x = static_cast<double>(start_x) + constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
-                    }
-                    if (direction == constants::Direction::WEST) {
-                        if (static_cast<double>(start_x) - calc_finish.x <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.x = calc_finish.x;
-                        } else {
-                            finish.x = static_cast<double>(start_x) - constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
+        Coordinates calc_finish = dog->GetCoordinateByTime(time_delta);
+        Coordinates finish;
+        constants::Direction direction = dog->GetDirection();
+        if (direction == constants::Direction::EAST || direction == constants::Direction::WEST) {
+            finish.y = start.y;
+            if (const Road* road_cur = map_->GetHorRoad(start_x, start_y)) {
+                if (direction == constants::Direction::EAST) {
+                    double road_x_end = static_cast<double>(road_cur->GetEnd().x);
+                    if (calc_finish.x - road_x_end <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.x = calc_finish.x;
+                    } else {
+                        finish.x = road_x_end + constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
                     }
                 }
-            } else if (direction == constants::Direction::NORTH || direction == constants::Direction::SOUTH) {
-                finish.x = start.x;
-                if (const Road* road_cur = map_->GetVerRoad(start_x, start_y)) {
-                    if (direction == constants::Direction::SOUTH) {
-                        double road_y_end = static_cast<double>(road_cur->GetEnd().y);
-                        if (calc_finish.y - road_y_end <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.y = calc_finish.y;
-                        } else {
-                            finish.y = road_y_end + constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
+                if (direction == constants::Direction::WEST) {
+                    double road_x_begin = static_cast<double>(road_cur->GetStart().x);
+                    if (road_x_begin - calc_finish.x <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.x = calc_finish.x;
+                    } else {
+                        finish.x = road_x_begin - constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
                     }
-                    if (direction == constants::Direction::NORTH) {
-                        double road_y_begin = static_cast<double>(road_cur->GetStart().y);
-                        if (road_y_begin - calc_finish.y <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.y = calc_finish.y;
-                        } else {
-                            finish.y = road_y_begin - constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
+                }
+            } else {
+                if (direction == constants::Direction::EAST) {
+                    if (calc_finish.x - static_cast<double>(start_x) <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.x = calc_finish.x;
+                    } else {
+                        finish.x = static_cast<double>(start_x) + constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
                     }
-                } else {
-                    if (direction == constants::Direction::SOUTH) {
-                        if (calc_finish.y - static_cast<double>(start_y) <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.y = calc_finish.y;
-                        } else {
-                            finish.y = static_cast<double>(start_y) + constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
-                    }
-                    if (direction == constants::Direction::NORTH) {
-                        if (static_cast<double>(start_y) - calc_finish.y <= constants::MAXDISTANCEFROMCENTER) {
-                            finish.y = calc_finish.y;
-                        } else {
-                            finish.y = static_cast<double>(start_y) - constants::MAXDISTANCEFROMCENTER;
-                            dog->SetSpeed({0, 0});
-                        }
+                }
+                if (direction == constants::Direction::WEST) {
+                    if (static_cast<double>(start_x) - calc_finish.x <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.x = calc_finish.x;
+                    } else {
+                        finish.x = static_cast<double>(start_x) - constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
                     }
                 }
             }
-            dog->SetCoordinate(finish);
+        } else if (direction == constants::Direction::NORTH || direction == constants::Direction::SOUTH) {
+            finish.x = start.x;
+            if (const Road* road_cur = map_->GetVerRoad(start_x, start_y)) {
+                if (direction == constants::Direction::SOUTH) {
+                    double road_y_end = static_cast<double>(road_cur->GetEnd().y);
+                    if (calc_finish.y - road_y_end <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.y = calc_finish.y;
+                    } else {
+                        finish.y = road_y_end + constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
+                    }
+                }
+                if (direction == constants::Direction::NORTH) {
+                    double road_y_begin = static_cast<double>(road_cur->GetStart().y);
+                    if (road_y_begin - calc_finish.y <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.y = calc_finish.y;
+                    } else {
+                        finish.y = road_y_begin - constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
+                    }
+                }
+            } else {
+                if (direction == constants::Direction::SOUTH) {
+                    if (calc_finish.y - static_cast<double>(start_y) <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.y = calc_finish.y;
+                    } else {
+                        finish.y = static_cast<double>(start_y) + constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
+                    }
+                }
+                if (direction == constants::Direction::NORTH) {
+                    if (static_cast<double>(start_y) - calc_finish.y <= constants::MAXDISTANCEFROMCENTER) {
+                        finish.y = calc_finish.y;
+                    } else {
+                        finish.y = static_cast<double>(start_y) - constants::MAXDISTANCEFROMCENTER;
+                        dog->SetSpeed({0, 0});
+                    }
+                }
+            }
         }
-
-    } catch (const std::exception& e) {
-           // Ловим исключения std::exception и добавляем информацию о функции
-           throw std::runtime_error(std::string("Exception in UpdateDogsCoordinatsByTime: ") + e.what());
-    } catch (...) {
-       // Ловим все остальные исключения и добавляем информацию о функции
-       throw std::runtime_error("Unknown exception in UpdateDogsCoordinatsByTime");
+        dog->SetCoordinate(finish);
     }
 
 }
 
 void GameSession::UpdateLootGenerationByTime(const std::chrono::milliseconds& time_delta) {
 
-    try{
+    unsigned loot_count = lost_objects_.size();
+    unsigned looter_count = dogs_.size();
 
+    unsigned new_loot_count = loot_generator_.Generate(time_delta, loot_count, looter_count);
 
-        unsigned loot_count = lost_objects_.size();
-        unsigned looter_count = dogs_.size();
-
-        unsigned new_loot_count = loot_generator_.Generate(time_delta, loot_count, looter_count);
-
-        for (unsigned i = 0; i < new_loot_count; ++i) {
-            auto lost_object = std::make_shared<LostObject>();
-            Point loot_coord = map_->GetRandomPointRoadMap();
-            //std::cout << "loot x = " << loot_coord.x << " y = " << loot_coord.y << std::endl;
-            lost_object->SetCoordinateByPoint(loot_coord);
-            lost_object->SetType(GetRandomTypeLostObject());
-            lost_objects_.insert(lost_object);
-        }
-
-    } catch (const std::exception& e) {
-        // Ловим исключения std::exception и добавляем информацию о функции
-        throw std::runtime_error(std::string("Exception in UpdateLootGenerationByTime: ") + e.what());
-    } catch (...) {
-        // Ловим все остальные исключения и добавляем информацию о функции
-        throw std::runtime_error("Unknown exception in UpdateLootGenerationByTime");
+    for (unsigned i = 0; i < new_loot_count; ++i) {
+        auto lost_object = std::make_shared<LostObject>();
+        Point loot_coord = map_->GetRandomPointRoadMap();
+        //std::cout << "loot x = " << loot_coord.x << " y = " << loot_coord.y << std::endl;
+        lost_object->SetCoordinateByPoint(loot_coord);
+        lost_object->SetType(GetRandomTypeLostObject());
+        lost_objects_.insert(lost_object);
     }
 
-
-//    net::dispatch(*game_session_strand_, [self = shared_from_this(), &time_delta]() {
-//            unsigned loot_count = self->lost_objects_.size();
-//            unsigned looter_count = self->dogs_.size();
-
-//            unsigned new_loot_count = self->loot_generator_.Generate(time_delta, loot_count, looter_count);
-
-//            for (unsigned i = 0; i < new_loot_count; ++i) {
-//                auto lost_object = std::make_shared<LostObject>();
-//                Point loot_coord = self->map_->GetRandomPointRoadMap();
-//                //std::cout << "loot x = " << loot_coord.x << " y = " << loot_coord.y << std::endl;
-//                lost_object->SetCoordinateByPoint(loot_coord);
-//                lost_object->SetType(self->GetRandomTypeLostObject());
-//                self->lost_objects_.insert(lost_object);
-//            }
-//    });
-    //net::dispatch(*api_strand, [self = shared_from_this(), &time_delta, &loot_count, &looter_count]() {
-//        unsigned new_loot_count = loot_generator_.Generate(time_delta, loot_count, looter_count);
-
-//        for (unsigned i = 0; i < new_loot_count; ++i) {
-//            auto lost_object = std::make_shared<LostObject>();
-//            lost_object->SetCoordinateByPoint(map_->GetRandomPointRoadMap());
-//            lost_object->SetType(GetRandomTypeLostObject());
-//            lost_objects_.insert(lost_object);
-//        }
-//        });
 }
 
 
