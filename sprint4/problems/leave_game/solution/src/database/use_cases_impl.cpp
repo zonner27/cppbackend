78b8603d91bff35c4ac22747acaf7800613a54f8
zonner27 <zonner2785@gmail.com>
2024-09-26 20:24:20 +0300

    sprint4 leave game

---
+++ sprint4/problems/leave_game/solution/src/database/use_cases_impl.cpp
@@ -0,0 +1,20 @@
+#include "use_cases_impl.h"
+
+#include <boost/uuid/uuid_io.hpp>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+
+namespace db_app {
+using namespace domain;
+
+void UseCasesImpl::AddRetiredPlayers(const std::vector<domain::RetiredPlayers> &retired_players) {
+    retired_players_.SaveRetiredPlayers(retired_players);
+}
+
+std::vector<RetiredPlayers> UseCasesImpl::GetTableRecords(size_t start, size_t maxItems) {
+    return retired_players_.GetTableRecord(start, maxItems);
+}
+
+}  // namespace app
